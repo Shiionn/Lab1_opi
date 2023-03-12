c71f5ebd9ce05f3abf8a9ad661094f4168af90e9
Shiionn <mazinova03@mail.ru>
2023-03-12 17:06:36 +0300

    lr3 trying to create new branches

---
+++ func3.h
@@ -0,0 +1,12 @@
+#ifndef func_H
+#define func_H
+
+#include <string>
+#include <string.h>
+
+extern std::string reverseWords(std::string);
+extern std::string join(std::string, std::string);
+extern std::string cut(std::string, int);
+
+#endif // !func_H
+
